1b1fa5abe15de066201a2e4ab3dc0348ecb766a4
Cory Dransfeldt <hi@coryd.dev>
2022-06-06 14:28:33 -0700

    update favicon assets

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,6 +1,6 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/blog', title: 'News' },
+  { href: '/tour', title: 'Tour' },
   { href: '/band', title: 'Band' },
   { href: '/media', title: 'Media' },
   { href: 'https://augurymetal.bandcamp.com/merch', title: 'Store' },
