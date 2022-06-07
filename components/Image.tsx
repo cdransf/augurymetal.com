5c1a068e2fb578350fe346f774cf45748eba2e9b
Cory Dransfeldt <hi@coryd.dev>
2022-06-07 12:59:19 -0700

    possible type fix

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,5 +1,6 @@
 import NextImage, { ImageProps } from 'next/image'
+import { ItemRef } from 'react-photoswipe-gallery'
 
-const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} />
+const Image = ({ ...rest }: ImageProps & { ref?: ItemRef }) => <NextImage {...rest} />
 
 export default Image
